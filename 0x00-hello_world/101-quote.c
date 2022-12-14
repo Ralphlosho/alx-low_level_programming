b13ff8d82438afd5bcc610660e376c199b8aba1c
Raoul <raoulfongue@gmail.com>
2022-12-13 16:29:42 -0800

    Fix 101-quote by using "write"

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -8,8 +9,7 @@
  */
 int main(void)
 {
-	int a=2;
-	int b=3;
-	if (a > b) perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return 1;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1)
 }
