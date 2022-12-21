acc1eb8647041a364c2395341d9b6e44d976f66d
Raoul <raoulfongue@gmail.com>
2022-12-20 16:35:53 -0800

    Write a code that prints the alphabet withouth "e" and "q"

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		if ((alph != 'e') && (alph != 'q'))
+		{
+			putchar(alph);
+			++alph;
+		}
+		else
+		{
+			++alph;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
