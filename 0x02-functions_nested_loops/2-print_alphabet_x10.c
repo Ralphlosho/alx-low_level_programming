4adf3c23d89eed6821946de78ddcabffe192f176
Raoul <raoulfongue@gmail.com>
2023-01-03 18:28:09 -0800

    task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase 
+ * folowed by \n
+ */
+void print_alphabet_x10(void)
+{
+	char alph;
+	int i;
+	i = 0;
+	
+	while (i < 10)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+			_putchar(alph);
+		_putchar('\n');
+	}
+
+}
