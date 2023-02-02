7ca3dce0e697c88a07bcd69781d08da23d635393
Raoul <raoulfongue@gmail.com>
2023-02-02 11:37:38 -0800

    Fix betty style in 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,8 +9,24 @@
 char *rot13(char *a)
 {
 	int i, j;
-	char *b = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM\
-		abcdefghijklmnopqrstuvwxyzabcdefghijklm";
+	char b[79] = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMabcdefghijklmnopqrstuvw";
+
+	b[62] = 120;
+	b[63] = 121;
+	b[64] = 122;
+	b[65] = 97;
+	b[66] = 98;
+	b[67] = 99;
+	b[68] = 100;
+	b[69] = 101;
+	b[70] = 102;
+	b[71] = 103;
+	b[72] = 104;
+	b[73] = 105;
+	b[74] = 106;
+	b[75] = 107;
+	b[76] = 108;
+	b[77] = 109;
 
 	j = 0;
 	while (a[j] != '\0')
@@ -18,7 +34,7 @@ char *rot13(char *a)
 		i = 0;
 		while (i < 65)
 		{
-			if (a[j] == b[i])
+			if (a[j] == b[i] && b[i])
 			{
 				a[j] = b[i + 13];
 				break;
